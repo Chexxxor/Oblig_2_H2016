1f403edad807c4a1e2770416f146f3570299c5e8
Kenneth Johan Kristensen <Chexxor@outlook.com>
2016-10-19 00:06:57 +0200

    Task 1, with cast error.

--- Oblig 2/src/task1/MergeSort.java
+++ Oblig 2/src/task1/MergeSort.java
@@ -3,10 +3,12 @@ package task1;
 public class MergeSort {
 	public static <E extends Comparable<E>> void mergeSort(E[] list){
 		if(list.length > 1){
+			@SuppressWarnings("unchecked")
 			E[] firstHalf = (E[]) new Comparable[list.length/2];
 			System.arraycopy(list, 0, firstHalf, 0, list.length/2);
 			mergeSort(firstHalf);
 	
+			@SuppressWarnings("unchecked")
 			E[] secondHalf = (E[]) new Comparable[list.length - list.length/2];
 			System.arraycopy(list, list.length/2, firstHalf, 0, list.length-list.length/2);
 			mergeSort(secondHalf);
@@ -15,13 +17,21 @@ public class MergeSort {
 		}
 	}
 	
-	public static <E> void merge(E[] first, E[] second, E[] result){
+	public static <E extends Comparable<E>> void merge(E[] first, E[] second, E[] result){
 		int current1 = 0;
 		int current2 = 0;
 		int current3 = 0;
 		
 		while(current1 < first.length && current2 < second.length){
-			
+			if((first[current1]).compareTo(second[current2]) < 0)
+			//if(first[current1].compareTo(second[current2]) < 0)
+				result[current3++] = first[current1++];
+			else
+				result[current3++] = second[current2++];
 		}
+		while(current1<first.length)
+			result[current3++] = first[current1++];
+		while(current2<second.length)
+			result[current3++] = second[current2++];
 	}
 }
